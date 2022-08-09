923727114d0afd62ae37b356092ccca1e1772848
Mike <mikhailvjacheslavovich@gmail.com>
2022-08-09 15:14:39 +0300

    Initial commit

---
+++ src/main/java/ru/inobitec/status/StatusApplication.java
@@ -0,0 +1,13 @@
+package ru.inobitec.status;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatusApplication.class, args);
+    }
+
+}
