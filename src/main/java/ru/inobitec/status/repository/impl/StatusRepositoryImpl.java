d5ea783b582d1e9445a88d8ca1ed040c81f30481
Admin <mikhailmukovnin@gmail.com>
2022-08-22 14:22:47 +0300

    fix

--- src/main/java/ru/inobitec/status/repository/impl/StatusRepositoryImpl.java
+++ src/main/java/ru/inobitec/status/repository/impl/StatusRepositoryImpl.java
@@ -1,6 +1,7 @@
 package ru.inobitec.status.repository.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 import ru.inobitec.status.mappers.StatusMapper;
 import ru.inobitec.status.model.StatusEntity;
@@ -8,12 +9,17 @@ import ru.inobitec.status.repository.StatusRepository;
 
 @Repository
 @RequiredArgsConstructor
+@Log4j2
 public class StatusRepositoryImpl implements StatusRepository {
 
     private final StatusMapper statusMapper;
 
     @Override
     public void changeStatus(StatusEntity status) {
-        statusMapper.changeStatus(status);
+        try{
+            statusMapper.changeStatus(status);
+        }catch(RuntimeException ex){
+            log.error(ex.getMessage());
+        }
     }
 }
